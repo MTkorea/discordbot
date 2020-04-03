22cbed5f617f4e2796f7c99b6a349f634c0a3fc8
MTkorea <37213919+MTkorea@users.noreply.github.com>
2020-04-03 15:32:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ async def on_message(message):
                             await i.send(embed=embed)
                     except:
                         pass
-            
+
     if message.content.startswith("!삭제"):
         await message.channel.purge(limit=100)
         await message.channel.send('메세지 100개 가삭제되었습니다.')
@@ -53,11 +53,54 @@ async def on_message(message):
             await message.channel.send(embed=embed)
             
     if message.content == '!디스코드':
-            embed=discord.Embed(title='Discord', color=0x0170ed, timestamp=message.created_at)
+            embed=discord.Embed(title='Discord', color=0x7289da, timestamp=message.created_at)
             embed.add_field(name="디스코드 주소", value=f'https://discord.gg/t5tZUUg', inline=True)
             embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
-            await message.channel.send(embed=embed)              
-        
+            await message.channel.send(embed=embed)
+
+    if message.content == '!핑':
+            embed=discord.Embed(title='초고퀄 최저가 프사샵', color=0x0170ed, timestamp=message.created_at)
+            embed.add_field(name="퐁!:ping_pong: ", value=f'봇이 정상적으로 동작중입니다.', inline=True)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+    if message.content == '!컬러테스트':
+            embed=discord.Embed(title='컬러테스트', color=0x35afd5, timestamp=message.created_at)
+            embed.add_field(name="컬러테스트", value=f'컬러테스트', inline=True)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+    if message.content == '!도움말':
+            embed=discord.Embed(title='초고퀄 최저가 프사샵', color=0x0170ed, timestamp=message.created_at)
+            embed.add_field(name="!내정보", value=f'간단한 자신의정보를 확인할수 있습니다.', inline=True)
+            embed.add_field(name="!디스코드", value=f'프사샵의 디스코드주소를 확인할수 있습니다.', inline=False)
+            embed.add_field(name="!배너문의", value=f'프사샵 배너문의를 하실수 있습니다.', inline=False)
+            embed.add_field(name="!핑", value=f'봇 정상 동작여부를 확인합니다.', inline=False)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+    if message.content == '!도움말':
+            embed=discord.Embed(title='대화봇', color=0x43b581, timestamp=message.created_at)
+            embed.add_field(name="대화봇 <할말> (Beta)", value=f'예) "대화봇 안녕" 등등 답을 해줍니다.', inline=False)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+    if message.content == '!디버그':
+            embed=discord.Embed(title='초고퀄 최저가 프사샵', color=0x0170ed, timestamp=message.created_at)
+            embed.add_field(name="!컬러테스트", value=f'임베드 컬러테스트', inline=True)
+            embed.add_field(name="!핑", value=f'봇 정상 동작여부를 확인합니다.', inline=False)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+#대화봇 디비부분
+
+    if message.content == '대화봇 안녕':
+            embed=discord.Embed(title='대화봇', color=0x0170ed, timestamp=message.created_at)
+            embed.add_field(name="응답", value=f'반갑습니다!', inline=True)
+            embed.set_footer(text=client.user.name, icon_url=client.user.avatar_url)
+            await message.channel.send(embed=embed)
+
+            
 async def my_background_task():
     await client.wait_until_ready()
     while not client.is_closed():
@@ -66,6 +109,9 @@ async def my_background_task():
         await asyncio.sleep(2)
         game = discord.Game("초고퀄프사 제작중")
         await client.change_presence(status=discord.Status.online, activity=game)
+        game = discord.Game("대화봇 구동중")
+        await client.change_presence(status=discord.Status.online, activity=game)
+        await asyncio.sleep(2)
         await asyncio.sleep(2)
         game = discord.Game(f'{len(client.guilds)}개의 서버에 참여중')
         await client.change_presence(status=discord.Status.online, activity=game)
